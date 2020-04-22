e6172f4565770dae7b6b8c3d387a77e4495bcda0
Morgan Thibert <morgan.thibert@gmail.com>
2020-04-22 21:37:00 +0200

    add script used for calibration during printing

---
+++ templates/basic-with-svg-export/optimizeGcode.py
@@ -0,0 +1,34 @@
+import sys
+
+def main() :
+    f = open( "D:/trashD/" + sys.argv[1], "r" )
+    fout = open( "D:/trashD/ "+ "optimized-"+ sys.argv[1], "w+")
+    lines = f.readlines()
+
+    isFound = False
+
+    # initialization auto home
+    fout.write("G28 \n")
+    # create a place where we put the pen at 0.5 height so that we can calibrate the pen
+    fout.write("G1 Z5.5 F1200 S0 \n")
+    fout.write("G1 X20.0 Y20.0 \n")
+    fout.write("G1 Z0.5 F1200 S255 \n")
+    fout.write("M18 \n")
+    fout.write("M0 \n")
+    fout.write("G1 Z5.5 F1200 S0 \n")
+    fout.write("G28 \n")
+    for line in lines:
+        currentLine = line.strip()
+        fout.write(currentLine + "\n")
+        if(isFound == False):
+            if ( currentLine.find("X") != -1 or currentLine.find("Y") != -1):
+                print(currentLine)
+                # M0 to wiat for insuer input
+                fout.write("M0 \n")
+                isFound = True
+
+
+    
+
+if __name__ == "__main__":
+    main()
