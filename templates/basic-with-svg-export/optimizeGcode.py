e78e1494a36f60aaa3793ee883596cf03679eeb8
Morgan Thibert <morgan.thibert@gmail.com>
2020-05-11 01:41:48 +0200

    correction in python script for generation of gcode

--- templates/basic-with-svg-export/optimizeGcode.py
+++ templates/basic-with-svg-export/optimizeGcode.py
@@ -93,24 +93,13 @@ def main() :
             fout.write( "; going outside \n")
             lineEq = calculateEqOfLine(prevX, prevY, currentX, currentY)
             # we are going outside we must calculate the point of intersection
-            intersectPt=[]
-            #  we must know which side we go
-            if(currentX <= minX): 
-                intersectX = minX
-                intersectY = lineEq[0] * intersectX + lineEq[1]
-                intersectPt=[intersectX,intersectY]
-            if(currentX >= maxX): 
-                intersectX=maxX
-                intersectY = lineEq[0] * intersectX + lineEq[1]
-                intersectPt=[intersectX,intersectY]
-            if(currentY <= minY): 
-                intersectY=minY
-                intersectX=(intersectY- lineEq[1])/lineEq[0]
-                intersectPt=[intersectX,intersectY]
-            if(currentY >= maxY):
-                intersectY=maxY
-                intersectX=(intersectY- lineEq[1])/lineEq[0]
-                intersectPt=[intersectX,intersectY]
+            intersectPt=getIntersectionPoint(currentX, currentY, lineEq)
+
+            # we check if point is valid and redo the interpolation if not
+            if (intersectPt[0] < minX or intersectPt[0] > maxX or intersectPt[1] < minY or intersectPt[1] > maxY ):
+                print("point non valide doing new intersect on ", intersectPt)
+                intersectPt=getIntersectionPoint(intersectPt[0], intersectPt[1], lineEq)
+                print(intersectPt)
 
             print(f"[->] Going outside at line {line_num}, from {prevX, prevY}, to { currentX, currentY },  intersect is : ", intersectPt)
             fout.write( "G0 X" + str(round(intersectPt[0],2)) + " Y"+ str(round(intersectPt[1],2)) + "; added Point\n")
@@ -122,35 +111,18 @@ def main() :
         # we are coming from outside we must calculate the point of intersection
             lineEq = calculateEqOfLine(prevX, prevY, currentX, currentY)
             fout.write("; comming from outside \n")
-            intersectPt=[]
-            #  we must know which side we go
-            if(prevX <= minX): 
-                intersectX = minX
-                intersectY = lineEq[0] * intersectX + lineEq[1]
-                intersectPt=[intersectX,intersectY]
+            intersectPt=getIntersectionPoint(prevX, prevY, lineEq)
 
-            if(prevX >= maxX): 
-                intersectX=maxX
-                intersectY = lineEq[0] * intersectX + lineEq[1]
-                intersectPt=[intersectX,intersectY]
-
-            if(prevY <= minY): 
-                intersectY=minY
-                intersectX=(intersectY- lineEq[1])/lineEq[0]
-                intersectPt=[intersectX,intersectY]
-
-            if(prevY >= maxY):
-                intersectY=maxY
-                intersectX=(intersectY- lineEq[1])/lineEq[0]
-                intersectPt=[intersectX,intersectY]
+            # we check if point is valid and redo the interpolation if not
+            if (intersectPt[0] < minX or intersectPt[0] > maxX or intersectPt[1] < minY or intersectPt[1] > maxY ):
+                print("point non valide doing new intersect on ", intersectPt)
+                intersectPt=getIntersectionPoint(intersectPt[0], intersectPt[1], lineEq)
+                print(intersectPt)
 
             print(f"[<-] Coming from outside at line {line_num}, from {prevX, prevY}, to {currentX, currentY}, intersect is : ", intersectPt)
             fout.write( "G0 X" + str(round(intersectPt[0],2)) + " Y"+ str(round(intersectPt[1],2)) + "; added Point\n")
             fout.write("G1 Z" + str(virtualZ)+ "; added Z \n")
             fout.write(currentLine + "\n")
-
-            
-        
 
         prevLine = currentLine
 
@@ -166,6 +138,39 @@ def main() :
 
     if(initializationFinished == False):
        print( "no initialization found !! Check settings in illustrator script.") 
+
+
+
+
+
+def getIntersectionPoint(pointX, pointY, lineEq):
+    intersectPt=[]
+    global minX
+    global maxX
+    global minY
+    global maxY
+    if(pointX < minX): 
+        intersectX = minX
+        intersectY = lineEq[0] * intersectX + lineEq[1]
+        intersectPt=[intersectX,intersectY]
+
+    elif(pointX > maxX): 
+        intersectX=maxX
+        intersectY = lineEq[0] * intersectX + lineEq[1]
+        intersectPt=[intersectX,intersectY]
+
+    elif(pointY < minY): 
+        intersectY=minY
+        intersectX=(intersectY- lineEq[1])/lineEq[0]
+        intersectPt=[intersectX,intersectY]
+
+    elif(pointY > maxY):
+        intersectY=maxY
+        intersectX=(intersectY- lineEq[1])/lineEq[0]
+        intersectPt=[intersectX,intersectY]
+
+    return intersectPt
+
 
 def getXYvalues(currentLine):
     xindex = currentLine.find("X")
