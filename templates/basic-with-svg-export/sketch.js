c2ff99607065073c6a6fc437bf57048a6cfdeb12
Morgan Thibert <morgan.thibert@gmail.com>
2020-05-25 17:24:43 +0200

    minor modif in sketch.js

--- templates/basic-with-svg-export/sketch.js
+++ templates/basic-with-svg-export/sketch.js
@@ -7,6 +7,7 @@ function preload() {
 
 function setup() {
     createCanvas(218 * 2, 292 * 2, SVG); // aspect ratio for printing on 24x32
+    noLoop();
     createSliders();
     background(255);
     strokeWeight(1); // for .5mm pen
@@ -14,14 +15,6 @@ function setup() {
     noFill();
 
     rect(0, 0, width, height)
-}
-
-function draw() {
-    count++;
     rect(random(width), random(height), s0(), s0());
-
-    if (count > 10) {
-        noLoop()
-    }
 
 }
