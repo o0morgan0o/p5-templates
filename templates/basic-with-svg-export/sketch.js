8596ab39479fdec70fe18bbf0f1ee640ec5b70e1
o0morgan0o <morgan.thibert@protonmail.com>
2020-09-04 01:33:31 +0200

    change dimensions and blend mode

--- templates/basic-with-svg-export/sketch.js
+++ templates/basic-with-svg-export/sketch.js
@@ -10,11 +10,12 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(218 * 2, 292 * 2, SVG); // aspect ratio for printing on 24x32
-    // createCanvas(218 * 2, 292 * 2); // aspect ratio for printing on 24x32
+    createCanvas(373 * 2, 455 * 2, SVG); // aspect ratio for printing on 24x32
+    blendMode(MULTIPLY)
+    // createCanvas(373 * 2, 455 * 2); // aspect ratio for printing on 24x32
     noLoop();
     // createSliders();
-    strokeWeight(1); // for .5mm pen
+    strokeWeight(.8); // for .4mm pen
     stroke(0);
     background(255);
     noFill();
